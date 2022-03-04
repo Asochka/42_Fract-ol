7928d030d376aa503b3615fe4b651c849df6eea1
Asochka <smana@il-g5.msk.21-school.ru>
2022-03-04 14:05:32 +0300

    start

---
+++ fractol.h
@@ -0,0 +1,10 @@
+#ifndef FRACTOL_H
+# define FRACTOL_H
+
+# include <mlx.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <math.h>
+
+#endif
---
+++ keys.h
@@ -0,0 +1,11 @@
+#ifndef KEYS_H
+# define KEYS_H
+
+# define KEY_ESCAPE			53
+
+# define KEY_A				0
+# define KEY_S				1
+# define KEY_D				2
+# define KEY_W				13
+
+#endif
