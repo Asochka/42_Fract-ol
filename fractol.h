0ec4055d0c8a419d81e1cec746057b7a2c645553
Asochka <fadeevafadeeva03@gmail.com>
2022-03-08 00:53:46 +0300

    с

--- fractol.h
+++ fractol.h
@@ -13,4 +13,32 @@
 
 # define THREADS	10
 
+typedef struct		s_complex
+{
+	double			re;
+	double			im;
+}					t_complex;
+
+typedef	struct		s_color
+{
+	int8_t			channel[4];
+}					t_color;
+
+/*
+** image          — image identifier
+** data_addr      — image
+** bits_per_pixel — depth of image
+** size_line      — number of bytes used to store one line of image
+** endian         — little or big endian
+*/
+
+typedef struct		s_image
+{
+	void			*image;
+	char			*data_addr;
+	int				bits_per_pixel;
+	int				size_line;
+	int				endian;
+}					t_image;
+
 #endif
--- main.c
+++ main.c
@@ -1,6 +1,28 @@
-#include <stdio.h>
+#include <fractol.h>
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
 
 int	main(int argc, char **argv)
 {
-	
+	void	*mlx;
+	void	*mlx_win;
+	void	*img;
+	t_image	img;
+
+	mlx = mlx_init();
+	img = mlx_new_image(mlx, WIDTH, HEIGHT);
+	mlx_win = mlx_new_window(mlx, WIDTH, HEIGHT);
+
+	img.data_addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+								&img.endian);
+	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	mlx_loop(mlx);
+}
 }
